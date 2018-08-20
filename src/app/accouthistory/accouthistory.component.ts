8895e83ac860042880055632a693836549070b17
pbareg <mail2praveen.b@gmail.com>
2018-08-20 22:45:15 +0530

    components registration,creditcard and account history

---
+++ src/app/accouthistory/accouthistory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accouthistory',
+  templateUrl: './accouthistory.component.html',
+  styleUrls: ['./accouthistory.component.css']
+})
+export class AccouthistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { CreditcardComponent } from './creditcard/creditcard.component';
+import { AccouthistoryComponent } from './accouthistory/accouthistory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    CreditcardComponent,
+    AccouthistoryComponent
   ],
   imports: [
     BrowserModule
