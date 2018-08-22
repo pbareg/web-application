30c4b25924eb24ae21f663b0c882a1c271480aa1
pbareg <mail2praveen.b@gmail.com>
2018-08-22 10:57:24 +0530

    updated code and new componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { CreditcardComponent } from './creditcard/creditcard.component';
 import { AccouthistoryComponent } from './accouthistory/accouthistory.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationComponent,
     CreditcardComponent,
-    AccouthistoryComponent
-  ],
+    AccouthistoryComponent,
+    LoginComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
+     ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent }
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
